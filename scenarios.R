8c334eae1c42f66c4fcfe8b195b4edd705965553
Raman A. Shah <raman@uchicago.edu>
2015-07-23 12:05:19 -0500

    scenarios.R: fix variable name.

--- scenarios.R
+++ scenarios.R
@@ -7,45 +7,51 @@ n.sample <- data.frame(num = c(2, 10, 50),
 
 for (i in 1:3) {
     # dsc-gtex-top10000
-    addScenario(dsc_gtex,name=paste0("null-top10000-", x$name[i]), fn=datamaker,
+    addScenario(dsc_gtex,name=paste0("null-top10000-", n.sample$name[i]),
+                fn=datamaker,
                 args=list(tissue="Adipose-Subcutaneous", path=path,
-                    Nsamp=x$num[i], Ngene=10000,
+                    Nsamp=n.sample$num[i], Ngene=10000,
                     voom.normalize=TRUE),
                 seed=1:50)
     
     # dsc-gtex-break-top10000
-    addScenario(dsc_gtex,name=paste0("null-break-top10000-", x$name[i]), fn=datamaker,
+    addScenario(dsc_gtex,name=paste0("null-break-top10000-", n.sample$name[i]),
+                fn=datamaker,
                 args=list(tissue="Adipose-Subcutaneous", 
-                    Nsamp=x$num[i], Ngene=10000, breaksample=TRUE, path=path,
+                    Nsamp=n.sample$num[i], Ngene=10000, breaksample=TRUE, path=path,
                     voom.normalize=TRUE),
                 seed=1:50)
 
     # dscr-gtex-diff-top10000
-    addScenario(dsc_gtex,name=paste0("alt-top10000-", x$name[i]), fn=datamaker,
+    addScenario(dsc_gtex,name=paste0("alt-top10000-", n.sample$name[i]),
+                fn=datamaker,
                 args=list(tissue=c("Adipose-Subcutaneous","Lung"), 
-                          Nsamp=x$num[i], Ngene=10000, breaksample=FALSE, path=path,
-                          voom.normalize=TRUE),
+                    Nsamp=n.sample$num[i], Ngene=10000, breaksample=FALSE,
+                    path=path, voom.normalize=TRUE),
                 seed=1:50)
 
     # dscr-gtex-break-diff-top10000
-    addScenario(dsc_gtex,name=paste0("alt-break-top10000-", x$name[i]), fn=datamaker,
+    addScenario(dsc_gtex,name=paste0("alt-break-top10000-", n.sample$name[i]),
+                fn=datamaker,
                 args=list(tissue=c("Adipose-Subcutaneous","Lung"), 
-                          Nsamp=x$num[i], Ngene=10000, breaksample=TRUE, path=path,
-                          voom.normalize=TRUE),
+                    Nsamp=n.sample$num[i], Ngene=10000, breaksample=TRUE,
+                    path=path, voom.normalize=TRUE),
                 seed=1:50)
 
     # dscr-gtex-break-mix-top10000
-    addScenario(dsc_gtex,name=paste0("mix-break-top10000-", x$name[i]), fn=datamaker,
+    addScenario(dsc_gtex,name=paste0("mix-break-top10000-", n.sample$name[i]),
+                fn=datamaker,
                 args=list(tissue=c("Adipose-Subcutaneous","Lung"), 
-                          Nsamp=x$num[i], Ngene=10000, path=path,
+                          Nsamp=n.sample$num[i], Ngene=10000, path=path,
                           voom.normalize=TRUE,
                           breaksample=TRUE, nullpi=0.9),
                 seed=1:5)
 
     # dscr-gtex-poisthin-top10000
-    addScenario(dsc_gtex,name=paste0("poisthin-top10000-", x$name[i]), fn=datamaker,
+    addScenario(dsc_gtex,name=paste0("poisthin-top10000-", n.sample$name[i]),
+                fn=datamaker,
                 args=list(tissue=c("Adipose-Subcutaneous"), 
-                          Nsamp=x$num[i], Ngene=10000, path=path,
+                          Nsamp=n.sample$num[i], Ngene=10000, path=path,
                           voom.normalize=TRUE,
                           poisthin=TRUE),
                 seed=1:5)
